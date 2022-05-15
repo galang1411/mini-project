99b19b14ebf2ae5bd10795736249f1e04a614d10
Galang Prang <galangprang1@gmail.com>
2022-05-15 21:55:09 +0800

    cicd

--- controllers/jadwal.go
+++ controllers/jadwal.go
@@ -51,7 +51,6 @@ func GetJadwalIDcontrollers(c echo.Context) error {
 	return c.JSON(http.StatusOK, helper.BuildResponse("success get jadwal", jadwal))
 }
 
-//
 func CreateJadwalscontrollers(c echo.Context) error {
 	jadwal := models.InsertJadwal{}
 	c.Bind(&jadwal)
