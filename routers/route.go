38f13f002c57de7203137c39090d6b2de421f20f
Galang Prang <galangprang1@gmail.com>
2022-05-16 21:09:19 +0800

    update

--- routers/route.go
+++ routers/route.go
@@ -18,30 +18,30 @@ func Router() *echo.Echo {
 	// Route to Dosen handler function
 	e.GET("/dosen", controllers.GetDosenscontrollers)
 	e.GET("/dosen/:nid", controllers.GetDosencontrollers)
-	e.POST("/dosen", controllers.CreateDosencontrollers)
-	e.DELETE("/dosen/:nid", controllers.DeleteDosencontrollers)
-	e.PUT("dosen/:nid", controllers.UpdateDosencontrollers)
+	eJwt.POST("/dosen", controllers.CreateDosencontrollers)
+	eJwt.DELETE("/dosen/:nid", controllers.DeleteDosencontrollers)
+	eJwt.PUT("dosen/:nid", controllers.UpdateDosencontrollers)
 
 	// Route to matkul handler function
 	e.GET("/matkul", controllers.GetMatkulscontrollers)
 	e.GET("/matkul/:id", controllers.GetMatkulcontrollers)
-	e.POST("/matkul", controllers.CreateMatkulcontrollers)
-	e.DELETE("/matkul/:id", controllers.DeleteMatkulcontrollers)
-	e.PUT("matkul/:id", controllers.UpdateMatkulcontrollers)
+	eJwt.POST("/matkul", controllers.CreateMatkulcontrollers)
+	eJwt.DELETE("/matkul/:id", controllers.DeleteMatkulcontrollers)
+	eJwt.PUT("matkul/:id", controllers.UpdateMatkulcontrollers)
 
 	// Route to ruangan handler function
 	e.GET("/ruangan", controllers.GetRuanganscontrollers)
 	e.GET("/ruangan/:id", controllers.GetRuangancontrollers)
-	e.POST("/ruangan", controllers.CreateRuangancontrollers)
-	e.DELETE("/ruangan/:id", controllers.DeleteRuangancontrollers)
-	e.PUT("ruangan/:id", controllers.UpdateRuangancontrollers)
+	eJwt.POST("/ruangan", controllers.CreateRuangancontrollers)
+	eJwt.DELETE("/ruangan/:id", controllers.DeleteRuangancontrollers)
+	eJwt.PUT("ruangan/:id", controllers.UpdateRuangancontrollers)
 
 	e.GET("/jadwal", controllers.GetJadwalscontrollers)
 	e.GET("/jadwal/hari/:hari", controllers.GetHariJadwalHaricontrollers)
 	e.GET("/jadwal/:id", controllers.GetJadwalIDcontrollers)
-	e.POST("/jadwal", controllers.CreateJadwalscontrollers)
-	e.PUT("/jadwal/update/:id", controllers.UpdateJadwalcontrollers)
-	e.DELETE("/jadwal/:id", controllers.DeleteJadwalcontrollers)
+	eJwt.POST("/jadwal", controllers.CreateJadwalscontrollers)
+	eJwt.PUT("/jadwal/update/:id", controllers.UpdateJadwalcontrollers)
+	eJwt.DELETE("/jadwal/:id", controllers.DeleteJadwalcontrollers)
 	return e
 
 	// go test ./controllers -coverprofile=coverage.out
