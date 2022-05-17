233a2a223319150dadf492da42ec0a9a26245181
Galang Prang <galangprang1@gmail.com>
2022-05-17 09:05:29 +0800

    update

--- controllers/ctrl_ruangan.go
+++ controllers/ctrl_ruangan.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"mini-project/config"
 	"mini-project/helper"
 	"mini-project/models"
@@ -72,26 +73,54 @@ func DeleteRuangancontrollers(c echo.Context) error {
 
 // update ruangan by id
 func UpdateRuangancontrollers(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	ruangan := models.Ruangan{}
-	if err := config.DB.Table("ruangan").First(&ruangan, id).Error; err != nil {
-		if err.Error() == "record not found" {
-			return c.JSON(http.StatusNotFound, map[string]interface{}{
-				"message": "ruangan not found",
-			})
-		}
-
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	id, err := strconv.Atoi(c.Param("code"))
+	if err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusBadRequest, "invalid id")
 	}
-
-	newruangan := models.Ruangan{}
-	c.Bind(&newruangan)
-
-	ruangan.Name = newruangan.Name
-
-	if err := config.DB.Table("ruangan").Save(&ruangan).Error; err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	fmt.Println("Isi userkId ", id)
+	var r models.Ruangan
+	fmt.Printf("Isi userk sebelum select %#v\n", r)
+	if err := config.DB.First(&r, id).Error; err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "internal server error")
 	}
+	if r.ID == 0 {
+		return c.String(http.StatusNotFound, "userk not found")
+	}
+	fmt.Printf("Isi userk setelah select %#v\n", r)
+	if err := c.Bind(&r); err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "internal server error")
+	}
+	fmt.Printf("Isi user setelah bind %#v\n", r)
+	fmt.Printf("Before update: %#v\n", r)
+	if err := config.DB.Save(&r).Error; err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "internal server error")
+	}
+	return c.JSON(http.StatusOK, r)
 
-	return c.JSON(http.StatusOK, helper.BuildResponse("success update ruangan", ruangan))
+	// id, _ := strconv.Atoi(c.Param("id"))
+	// ruangan := models.Ruangan{}
+	// if err := config.DB.Table("ruangan").First(&ruangan, id).Error; err != nil {
+	// 	if err.Error() == "record not found" {
+	// 		return c.JSON(http.StatusNotFound, map[string]interface{}{
+	// 			"message": "ruangan not found",
+	// 		})
+	// 	}
+
+	// 	return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	// }
+
+	// newruangan := models.Ruangan{}
+	// c.Bind(&newruangan)
+
+	// ruangan.Name = newruangan.Name
+
+	// if err := config.DB.Table("ruangan").Save(&ruangan).Error; err != nil {
+	// 	return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	// }
+
+	// return c.JSON(http.StatusOK, helper.BuildResponse("success update ruangan", ruangan))
 }
